1f7458da46ed3004ef4430dcf50d5dbd9ea032e6
Jason Falk <jasonfalk9@gmail.com>
2024-10-09 09:45:05 -0700

    Make reducer return a default state if no action is passed into reducer.

---
+++ src/_tests_/reducers/merch-site-reducer.test.js
@@ -0,0 +1,71 @@
+import merchSiteReducer from "../../reducers/merch-site-reducer";
+
+describe("merchSiteReducer", () => {
+
+  const currentState = {
+    1: {
+      name: "test1",
+      description: "description1 test1",
+      price: 1,
+      quantity: 1,
+      id: 1
+    }, 2: {
+      name: "test2",
+      description: "description2 test2",
+      price: 2,
+      quantity: 2,
+      id: 2
+    }
+  }
+
+  let action;
+  const itemData = {
+    name: "test",
+    description: "description test",
+    price: 15,
+    quantity: 15,
+    id: 1
+  };
+
+  test("Should return default state if there is no action type passed into the reducer", () => {
+    expect(merchSiteReducer({}, { type: null })).toEqual({});
+  });
+
+  // test("Should successfully add new item data to mainItemList", () => {
+  //   const { name, description, price, quantity, id } = itemData;
+  //   action = {
+  //     type: "ADD_ITEM",
+  //     name: name,
+  //     description: description,
+  //     price: price,
+  //     quantity: quantity,
+  //     id: id
+  //   };
+
+  //   expect(merchSiteReducer({}, action)).toEqual({
+  //     [id] : {
+  //       name: name,
+  //       description: description,
+  //       price: price,
+  //       quantity: quantity,
+  //       id: id
+  //     }
+  //   });
+  // });
+
+  // test("Should successfully delete an item", () => {
+  //   action = {
+  //     type: "DELETE_ITEM",
+  //     id: 1
+  //   };
+  //   expect(merchSiteReducer(currentState, action)).toEqual({
+  //     2: {
+  //       name: "test2",
+  //       description: "description2 test2",
+  //       price: 2,
+  //       quantity: 2,
+  //       id: 2
+  //     }
+  //   });
+  // });
+});
---
+++ src/reducers/merch-site-reducer.js
@@ -0,0 +1,23 @@
+const reducer = (state = {}, action) => {
+  const { name, description, price, quantity, id } = action;
+  switch (action.type) {
+    case "ADD_ITEM":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          price: price,
+          quantity: quantity,
+          id: id
+        }
+      });
+    case "DELETE_ITEM":
+      let newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
